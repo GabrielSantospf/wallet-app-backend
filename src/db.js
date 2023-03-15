b2eb7e01d22d45adb35004eae22f115a7677279d
GabrielSantospf <gabrielspferreira@hotmail.com>
2023-03-15 19:58:11 -0300

    Create routes for categories, mainly post

--- src/db.js
+++ src/db.js
@@ -1,11 +1,11 @@
 const { Pool } = require("pg");
 
-const db = new Pool ({
-    user: "docker",
-    password: "docker",
-    database: "finances",
-    host: "localhost",
-    port: 5432,
-})
+const db = new Pool({
+  user: "docker",
+  password: "docker",
+  database: "finances",
+  host: "localhost",
+  port: 5432,
+});
 
 module.exports = db;
--- src/index.js
+++ src/index.js
@@ -1,31 +1,25 @@
-const { response } = require("express")
-const express = require("express")
-const db = require("./db")
+const express = require("express");
+const db = require("./db");
+const routesCategories = require("./routes/categories");
 
-const app = express()
-const port = 3000
+const app = express();
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Olá, essa é a aplicação Wallet App!')
-})
+const port = 3000;
 
-app.get("/categories", (req, res) => {
-  db.query("SELECT * FROM categories", (error, response) => {
-    if(error) {
-      return res.status(500).json(error);
-    } 
+app.get("/", (req, res) => {
+  res.send("Olá, essa é a aplicação Wallet App!");
+});
 
-      return res.status(200).json(response.rows);
-
-  })
-})
+app.use("/categories", routesCategories);
 
 app.listen(port, () => {
   db.connect()
     .then(() => {
-      console.log("DB connected")
-  }) .catch((error) => {
-    throw new Error(error);
-  });
-  console.log(`Example app listening on port ${port}`)
+      console.log("DB connected");
+    })
+    .catch((error) => {
+      throw new Error(error);
+    });
+  console.log(`Example app listening on port ${port}`);
 });
